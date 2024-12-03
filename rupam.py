f10f10a57cc4c7a6f2041df40f44eed3f029e78e
BGMI762 <bgmiddosbot89@gmail.com>
2024-12-03 23:04:11 +0530

    Update rupam.py

--- rupam.py
+++ rupam.py
@@ -5,7 +5,7 @@ import datetime
 import os
 
     
-bot = telebot.TeleBot('6978866468:AAGLcR0qILP7EXlM6v90lQUkbdGRgkv2y2I')
+bot = telebot.TeleBot('7276022472:AAGoyTYUhrBOUieBJr1CSGX8abUrirhPUTg')
 admin_id = ["6804992562"]
 
 
